88bd5952035daf90354fe3bb8c16b8de3e6b4341
akowalik95 <a.kowalik.1702@gmail.com>
2018-04-19 12:17:54 +0200

    Walka z gitem

--- ProjektIP/Common/User.cs
+++ ProjektIP/Common/User.cs
@@ -9,16 +9,25 @@ namespace ProjektIP.Common
 	public class User
 	{
 		public long Id;
-		private string login;
+		public string Login;
+		public List<long> UserPermission;
+
+		public User()
+		{
+			Id = 0;
+		}
 
 		public User(string login, string password)
 		{
-			this.login = login;
+			this.Login = login;
 			Dictionary<string, object> filter = new Dictionary<string, object>();
 			filter.Add("Login", login);
 			filter.Add("Password", password);
 			List<object[]> list = BaseDAO.Select("Users", new List<string> { "IdUser", "Login", "Password", "IdEmployee" }, filter);
 			this.Id = Convert.ToInt64(list[0][3]);
+			List<object[]> permissions = BaseDAO.Select("EmployeePermissions", new List<string> { "IdEmployee", "IdPermission" }, new Dictionary<string, object>() { { "IdEmployee", Id } });
+			List<long> userPermissions = permissions.Select(x => Convert.ToInt64(x[1])).ToList();
+			UserPermission = userPermissions;
 		}
 	}
 }
