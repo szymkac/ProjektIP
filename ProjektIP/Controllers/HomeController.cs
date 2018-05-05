b6ec4d0ef47cffcc494eace3a4f42c5d43b4f349
MonikaCalka <monika.calka95@gmail.com>
2018-05-05 10:49:33 +0200

    Dodanie avatarów we właściwe miejsca (bez uwzględnienia znikania na logowaniu)

--- ProjektIP/Controllers/HomeController.cs
+++ ProjektIP/Controllers/HomeController.cs
@@ -10,59 +10,64 @@ using ProjektIP.Models;
 
 namespace ProjektIP.Controllers
 {
-	public class HomeController : Controller
-	{
-		public static User ActualUser;
-		public IActionResult Info()
-		{
-			return View();
-		}
-		public IActionResult Index(bool unCorrectData)
-		{
-			if (unCorrectData)
-			{
-				ViewBag.notLogIn = "Niepoprawny login lub hasło";
-			}
-			else
-				ViewBag.notLogIn = "OK";
+    public class HomeController : Controller
+    {
+        public static User ActualUser;
+        public static long? ActualPosition;
+        public IActionResult Info()
+        {
+            return View();
+        }
+        public IActionResult Index(bool unCorrectData)
+        {
+            if (unCorrectData)
+            {
+                ViewBag.notLogIn = "Niepoprawny login lub hasło";
+            }
+            else
+                ViewBag.notLogIn = "OK";
 
-			if (ActualUser == null)
-				return View();
+            if (ActualUser == null)
+                return View();
 
-			return RedirectToAction("MainPage", "Home");
-		}
-		[HttpPost]
-		public IActionResult Login(string login, string password)
-		{
-			if (AccountController.AccountDAO.CheckUser(login, password))
-			{
-				ActualUser = new User(login, password);
-				return RedirectToAction("MainPage", "Home");
-			}
-			ActualUser = null;
-			return RedirectToAction("Index", "Home", new { unCorrectData = true });
-		}
-		public IActionResult UserInfo()
-		{
-			ViewBag.loggedIn = ActualUser;
-			return PartialView();
-		}
-		public IActionResult Logout()
-		{
-			ActualUser = null;
-			return RedirectToAction("Index", "Home");
-		}
-		public IActionResult MainPage()
-		{
-			if (ActualUser != null)
-				return View("MainPage", ActualUser.UserPermission);
-			return RedirectToAction("Index", "Home");
-		}
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-
-
-	}
+            return RedirectToAction("MainPage", "Home");
+        }
+        [HttpPost]
+        public IActionResult Login(string login, string password)
+        {
+            if (AccountController.AccountDAO.CheckUser(login, password))
+            {
+                ActualUser = new User(login, password);
+                return RedirectToAction("MainPage", "Home");
+            }
+            ActualUser = null;
+            return RedirectToAction("Index", "Home", new { unCorrectData = true });
+        }
+        public IActionResult UserInfo()
+        {
+            ViewBag.loggedIn = ActualUser;
+            ViewBag.actualPosition = ActualPosition;
+            return PartialView();
+        }
+        public IActionResult Logout()
+        {
+            ActualUser = null;
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult MainPage()
+        {
+            if (ActualUser != null)
+            {
+                List<EmployeeModel> ActualEmployee = EmployeeController.GetEmployeeWithId(ActualUser.Id);
+                ActualPosition = ActualEmployee[0].PositionId;
+                ViewBag.ActualPosition = ActualPosition;
+                return View("MainPage", ActualUser.UserPermission);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
